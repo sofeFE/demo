848c229a49602160133002a5cf347ad34808439f
java-GD <42560118+java-GD@users.noreply.github.com>
2019-06-03 15:40:39 +0800

    Initial commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@MapperScan(value="com.example.demo.*.dao")
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+
+}
---
+++ target/classes/META-INF/resources/resources.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2019/4/5
+  Time: 3:34
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>meta-inf/resources</title>
+</head>
+<body>
+this is test jsp!
+</body>
+</html>
